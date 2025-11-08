ba58f5a28251045219b488eeb84d4b610e7f230c
Ian <mutantx22@live.ca>
2025-11-08 01:58:57 -0500

    Add files via upload

---
+++ preventclose.js
@@ -0,0 +1,32 @@
+var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opens = _____WB$wombat$assign$function_____("opens");
+window.onbeforeunload = function() { return "Would you really like to close your browser?"; }
+
+}
+/*
+     FILE ARCHIVED ON 21:44:37 Jul 31, 2025 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 04:18:47 Nov 08, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.553
+  exclusion.robots: 0.023
+  exclusion.robots.policy: 0.011
+  esindex: 0.009
+  cdx.remote: 24.123
+  LoadShardBlock: 117.266 (3)
+  PetaboxLoader3.datanode: 121.111 (4)
+  load_resource: 205.818
+  PetaboxLoader3.resolve: 163.374
+*/
